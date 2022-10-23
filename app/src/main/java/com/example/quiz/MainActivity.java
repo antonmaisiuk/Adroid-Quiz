4f1828c84c04a518128ef4b5bfc2352e161a0d66
Anton <superantoxa12@gmail.com>
2022-10-23 14:36:13 +0200

    Addiction to the previuos commit.

--- app/src/main/java/com/example/quiz/MainActivity.java
+++ app/src/main/java/com/example/quiz/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.quiz;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.nfc.Tag;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,7 +14,21 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String KEY_CURRENT_INDEX = "currentIndex";
+    public boolean answerWasShown;
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode != RESULT_OK){ return;}
+        if (requestCode == REQUEST_CODE_PROMPT){
+            if (data == null){ return; }
+            answerWasShown = data.getBooleanExtra(PromptActivity.KEY_EXTRA_ANSWER_SHOW, false);
+        }
+    }
+
+    public static final String KEY_EXTRA_ANSWER = "com.example.quiz.correctAnswer";
+    public static final int REQUEST_CODE_PROMPT = 0;
     private TextView questionTextView;
     private int currentIndex = 0;
 
@@ -26,17 +43,29 @@ public class MainActivity extends AppCompatActivity {
     private void checkAnswerCorrectness(boolean userAnswer){
         boolean correctAnswer = questions[currentIndex].isTrueAnswer();
         int resultMessageId;
-        if(userAnswer == correctAnswer){
-            resultMessageId = R.string.correct_answer;
-        } else{
-            resultMessageId = R.string.incorrect_answer;
+        if (answerWasShown){
+            resultMessageId = R.string.answer_was_shown;
+        } else {
+            if(userAnswer == correctAnswer){
+                resultMessageId = R.string.correct_answer;
+            } else{
+                resultMessageId = R.string.incorrect_answer;
+            }
         }
+
         Toast.makeText(this,resultMessageId,Toast.LENGTH_SHORT).show();
     }
 
 
     private void setNextQuestion(){
         questionTextView.setText(questions[currentIndex].getQuestionId());
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.d("TAG", "Wywołana metoda onSaveInstanceState");
+        outState.putInt(KEY_CURRENT_INDEX, currentIndex);
     }
 
     @Override
@@ -72,22 +101,37 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("TAG", "onCreate");
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState != null){
+            currentIndex = savedInstanceState.getInt(KEY_CURRENT_INDEX);
+        }
+
 
         Button trueButton;
         Button falseButton;
         Button nextButton;
+        Button promptButton;
 
         trueButton = findViewById(R.id.true_btn);
         falseButton = findViewById(R.id.false_btn);
         nextButton = findViewById(R.id.next_btn);
+        promptButton = findViewById(R.id.answer_btn_main_activity);
         questionTextView = findViewById(R.id.quest_text_view);
 
         trueButton.setOnClickListener(v -> checkAnswerCorrectness(true));
         falseButton.setOnClickListener(v -> checkAnswerCorrectness(false));
+        promptButton.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, PromptActivity.class);
+            boolean correctAnswer = questions[currentIndex].isTrueAnswer();
+            intent.putExtra(KEY_EXTRA_ANSWER, correctAnswer);
+            startActivityForResult(intent, REQUEST_CODE_PROMPT);
+        });
 
         nextButton.setOnClickListener(v -> {
             currentIndex = (currentIndex + 1)%questions.length;
+            answerWasShown = false;
             setNextQuestion();
         });
         setNextQuestion();
--- app/src/main/java/com/example/quiz/PromptActivity.java
+++ app/src/main/java/com/example/quiz/PromptActivity.java
@@ -2,13 +2,40 @@ package com.example.quiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class PromptActivity extends AppCompatActivity {
+
+    private boolean correctAnswer;
+    public static final String KEY_EXTRA_ANSWER_SHOW = "com.example.quiz.answerShown";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prompt);
+
+        Button showCorrectAnswerButton;
+        TextView answerTextView;
+
+        showCorrectAnswerButton = findViewById(R.id.answer_btn);
+        answerTextView = findViewById(R.id.answer_text_view);
+
+        correctAnswer = getIntent().getBooleanExtra(MainActivity.KEY_EXTRA_ANSWER, true);
+
+
+        showCorrectAnswerButton.setOnClickListener(view -> {
+            int answer = correctAnswer ? R.string.true_button : R.string.false_button;
+            answerTextView.setText(answer);
+            setAnswerShownResult(true);
+        });
+
+    }
+    public void setAnswerShownResult(boolean answerWasShown){
+        Intent resultIntent = new Intent();
+        resultIntent.putExtra(KEY_EXTRA_ANSWER_SHOW, answerWasShown);
+        setResult(RESULT_OK, resultIntent);
     }
 }
